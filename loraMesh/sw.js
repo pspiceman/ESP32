be410a85857d5978ada945ef2e43c78a9c02c3bd
pspiceman <pspicemaster@gmail.com>
2026-01-08 19:53:37 +0900

    Add files via upload

---
+++ loraMesh/sw.js
@@ -0,0 +1,29 @@
+const CACHE_NAME = "lora-mesh-pwa-v1";
+const ASSETS = [
+  "/loraMesh/",
+  "/loraMesh/loraMesh.html",
+  "/loraMesh/manifest.webmanifest",
+  "/loraMesh/loraMesh.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.map((k) => (k !== CACHE_NAME ? caches.delete(k) : null)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
+  );
+});
