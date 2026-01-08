d7c352f07b1690570462b3a3a31d269c29cc131e
pspiceman <pspicemaster@gmail.com>
2026-01-09 08:33:35 +0900

    Add files via upload

--- loraMesh/sw.js
+++ loraMesh/sw.js
@@ -1,12 +1,11 @@
 const CACHE_NAME = "lora-mesh-pwa-v2";
 const ASSETS = [
-  "/loraMesh/",
-  "/loraMesh/loraMesh.html",
-  "/loraMesh/manifest.json",
-  "/loraMesh/loraMesh.png"
+  "./",
+  "./loraMesh.html",
+  "./manifest.webmanifest",
+  "./loraMesh.png"
 ];
 
-// 설치 시 캐시
 self.addEventListener("install", (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
@@ -14,7 +13,6 @@ self.addEventListener("install", (event) => {
   self.skipWaiting();
 });
 
-// 오래된 캐시 정리
 self.addEventListener("activate", (event) => {
   event.waitUntil(
     caches.keys().then((keys) =>
@@ -24,7 +22,6 @@ self.addEventListener("activate", (event) => {
   self.clients.claim();
 });
 
-// 네트워크 우선, 실패 시 캐시
 self.addEventListener("fetch", (event) => {
   event.respondWith(
     fetch(event.request).catch(() => caches.match(event.request))
