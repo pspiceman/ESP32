78c0348bd24d1fa17594ad8e9d2f0a46e7c025d7
pspiceman <pspicemaster@gmail.com>
2026-03-02 12:16:03 +0900

    Add files via upload

---
+++ trade/sw.js
@@ -0,0 +1,43 @@
+const CACHE_NAME = "kospi-calendar-android-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.webmanifest",
+  "./sw.js",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png",
+  "./icons/icon-192-maskable.png",
+  "./icons/icon-512-maskable.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.map((k) => (k === CACHE_NAME ? null : caches.delete(k))))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  const req = event.request;
+  event.respondWith(
+    caches.match(req).then((cached) => {
+      if (cached) return cached;
+      return fetch(req).then((res) => {
+        try {
+          const url = new URL(req.url);
+          if (url.origin === self.location.origin) {
+            caches.open(CACHE_NAME).then((cache) => cache.put(req, res.clone()));
+          }
+        } catch (e) {}
+        return res;
+      }).catch(() => caches.match("./index.html"));
+    })
+  );
+});
