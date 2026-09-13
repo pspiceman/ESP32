cf73fb08fc19d40ffd5459df486704815f7da9a9
pspiceman <pspicemaster@gmail.com>
2026-09-13 13:00:12 +0900

    Add files via upload

--- loraMesh/sw.js
+++ loraMesh/sw.js
@@ -1,38 +1,33 @@
-const CACHE_NAME = 'lora-mesh-monitor-v1';
+const CACHE_NAME = 'lora-mesh-pwa-v2';
 const APP_SHELL = [
+  './',
   './loraMesh.html',
   './manifest.webmanifest',
-  './loraMesh.png',
   './loraMesh-192.png',
   './loraMesh-512.png',
   './loraMesh-maskable-512.png'
 ];
 
 self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(APP_SHELL))
-      .then(() => self.skipWaiting())
-  );
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL)));
+  self.skipWaiting();
 });
 
 self.addEventListener('activate', event => {
   event.waitUntil(
-    caches.keys()
-      .then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))))
-      .then(() => self.clients.claim())
+    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))))
   );
+  self.clients.claim();
 });
 
 self.addEventListener('fetch', event => {
-  if (event.request.method !== 'GET') return;
+  const req = event.request;
+  if (req.method !== 'GET') return;
   event.respondWith(
-    fetch(event.request)
-      .then(response => {
-        const copy = response.clone();
-        caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy));
-        return response;
-      })
-      .catch(() => caches.match(event.request).then(r => r || caches.match('./loraMesh.html')))
+    fetch(req).then(res => {
+      const copy = res.clone();
+      caches.open(CACHE_NAME).then(cache => cache.put(req, copy));
+      return res;
+    }).catch(() => caches.match(req).then(cached => cached || caches.match('./loraMesh.html')))
   );
 });
